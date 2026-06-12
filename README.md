f73e635c1288af6d319d544e298d6d55bc7b75ba
Caio Marcos <caio.maciel@germinare.org.br>
2026-06-11 21:05:13 -0300

    feat(repos): creating a script to clone all repositories from Aether's git hub

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# aether-toolkit
-Ambiente central de infraestrutura e orquestração do ecossistema Aether, responsável pela inicialização integrada dos serviços, containers, variáveis de ambiente e ferramentas auxiliares.
+# aether-toolskit
+Ambiente para orquestração dos containeres utilizados pelo projeto Aether, onde concentram-se containeres do Docker que permitem a execução e teste de todas as APIS do projeto, além de ser um facilitador para clonar todos os repositórios.
+
+### Como clonar todos os repositórios?
+```
+./scripts/repos.ps1
+```
+Esse comando clonará todos os repositórios do projeto para a pasta resources
---
+++ scripts/repos.ps1
@@ -0,0 +1,35 @@
+$resources = Join-Path $PSScriptRoot "./../resources"
+
+$repositories = @(
+    "https://github.com/AetherGases/aether-web-flow.git",
+    "https://github.com/AetherGases/aether-docs.git",
+    "https://github.com/AetherGases/aether-analytics.git",
+    "https://github.com/AetherGases/aether-landing.git",
+    "https://github.com/AetherGases/aether-web-administrative.git",
+    "https://github.com/AetherGases/aether-toolskit.git",
+    "https://github.com/AetherGases/aether-core-api.git",
+    "https://github.com/AetherGases/aether-mobile.git",
+    "https://github.com/AetherGases/aether-ai-gateway.git",
+    "https://github.com/AetherGases/aether-ai-sdk.git"
+)
+
+if (-not (Test-Path $resources)) {
+    New-Item -ItemType Directory -Path $resources -Force | Out-Null
+}
+
+
+Write-Host "Iniciando clonagem"
+
+foreach ($repository in $repositories) {
+    $name = [System.IO.Path]::GetFileNameWithoutExtension($repository)
+    $destination = Join-Path $resources $name
+    
+    if (Test-Path $destination) {
+        Write-Host "$name ja existe, pulando..."
+        continue
+    }
+
+    git clone $repository $destination
+}
+
+Write-Host "Clonagem concluida"
