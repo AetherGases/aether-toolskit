e0bbbe1a1667a5af35430e9c560fc6ed2c5fdf27
Caio Marcos <caio.maciel@germinare.org.br>
2026-08-15 16:58:41 -0300

    feat: adding a initial mongo script to create collection send_code

--- init-mongo/init.js
+++ init-mongo/init.js
@@ -1,22 +1,39 @@
 // Example of init mongo code
 db = db.getSiblingDB("dbAether")
 
-db.createCollection("chats", {
+db.createCollection("send_code", {
   validator: {
     $jsonSchema: {
       bsonType: "object",
       required: [
-        "chatId",
-        "userId"
+        "user_id",
+        "email",
+        "code",
+        "validated",
+        "created_at"
       ],
       properties: {
-        chatId: {
+        user_id: {
+          bsonType: "int"
+        },
+        email: {
           bsonType: "string"
         },
-        userId: {
+        validated: {
+          bsonType: "bool"
+        },
+        code: {
           bsonType: "string"
+        },
+        created_at: {
+          bsonType: "date"
         }
       }
     }
   }
 })
+
+db.send_code.createIndex(
+  { "created_at": 1 },
+  { expireAfterSeconds: 1800 }
+)
